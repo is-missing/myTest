e158fb1ebb7891d8f5264413168197aeb547aab8
小李不见了 <11722237+xiao-li-is-missing@user.noreply.gitee.com>
2023-04-06 20:44:17 +0800

    create aa

---
+++ my-app/src/components/aa.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>page</div>
+</template>
+
+<script setup>
+import { ref, reactive, toRefs } from 'vue'
+import { useRoute, useRouter } from 'vue-router';
+const router = useRouter()
+const route = useRoute()
+const data = reactive({
+
+})
+</script>
+
+<style scoped></style>
