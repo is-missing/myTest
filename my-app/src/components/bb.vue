58717939c100fba38b5f04e29d2801230eb9036c
小李不见了 <11722237+xiao-li-is-missing@user.noreply.gitee.com>
2023-04-06 20:47:22 +0800

    add bb

---
+++ my-app/src/components/bb.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        这是bb组件
+    </div>
+</template>
+
+<script setup>
+import { ref, reactive, toRefs } from 'vue'
+import { useRoute, useRouter } from 'vue-router';
+const router = useRouter()
+const route = useRoute()
+const data = reactive({
+
+})
+</script>
+
+<style scoped></style>
